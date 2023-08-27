c047167c553cde85daba541e8526d2525c98b280
Kirill Suranov <suranov123w@gmail.com>
2023-08-27 21:45:48 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use('/marvel-api', express.static(__dirname + "/public/marvel-api"));
+
+app.all("*", (req, res) => {
+  res.status(404).json("resource not found")
+})
+
+app.listen(port, console.log(`server is listening on the port ${port}`));
